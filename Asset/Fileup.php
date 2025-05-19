567ba8d9e94b9d666e2b1b616894aac6d44d2bf4
Mahir Shariar Ratul <mahirratul2002@gmail.com>
2025-05-19 13:25:34 +0600

    Fileup.php

--- Asset/Fileup.php
+++ Asset/Fileup.php
@@ -3,25 +3,24 @@
 if (isset($_FILES['myfile'])) {
     $src = $_FILES['myfile']['tmp_name'];
 
-    // Get file extension safely
+  
     $ext = pathinfo($_FILES['myfile']['name'], PATHINFO_EXTENSION);
 
-    // Create a unique filename
     $fileName = 'uploaded_' . time() . '.' . $ext;
 
-    // Absolute path to the upload directory
+    
     $uploadDir =  'C:/xampp/htdocs/Employee-Management-System/Asset/upload/';
 ;
 
-    // Ensure the upload directory exists
+
     if (!file_exists($uploadDir)) {
         mkdir($uploadDir, 0777, true);
     }
 
-    // Define destination path AFTER setting $fileName and $uploadDir
+   
     $des = $uploadDir . $fileName;
 
-    // Now move the uploaded file
+  
     if (move_uploaded_file($src, $des)) {
         echo "Success: File uploaded as $fileName";
     } else {
