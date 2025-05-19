54108fae3ba0f060b1a77da8c946d2a517f7d0dd
MAHIR178 <mahirratul2002@gmail.com>
2025-05-19 19:05:21 +0600

    commit

--- Asset/Fileup.php
+++ Asset/Fileup.php
@@ -3,41 +3,24 @@
 if (isset($_FILES['myfile'])) {
     $src = $_FILES['myfile']['tmp_name'];
 
-<<<<<<< HEAD
 
     $ext = pathinfo($_FILES['myfile']['name'], PATHINFO_EXTENSION);
 
    
-=======
-  
-    $ext = pathinfo($_FILES['myfile']['name'], PATHINFO_EXTENSION);
-
->>>>>>> 567ba8d9e94b9d666e2b1b616894aac6d44d2bf4
     $fileName = 'uploaded_' . time() . '.' . $ext;
 
     
     $uploadDir =  'C:/xampp/htdocs/Employee-Management-System/Asset/upload/';
 ;
 
-<<<<<<< HEAD
     
-=======
-
->>>>>>> 567ba8d9e94b9d666e2b1b616894aac6d44d2bf4
     if (!file_exists($uploadDir)) {
         mkdir($uploadDir, 0777, true);
     }
 
-<<<<<<< HEAD
     
     $des = $uploadDir . $fileName;
 
-=======
-   
-    $des = $uploadDir . $fileName;
-
-  
->>>>>>> 567ba8d9e94b9d666e2b1b616894aac6d44d2bf4
     if (move_uploaded_file($src, $des)) {
         echo "Success: File uploaded as $fileName";
     } else {
